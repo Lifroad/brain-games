91dd93aa04a9096d7fbc9e5c0893a3c9fa8e7616
Lifroad <jlifroad@gmail.com>
2024-05-10 01:49:29 +0300

    remove files

--- src/cli.js
+++
@@ -1,8 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const userName = () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-};
-
-export default userName;
